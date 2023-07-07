54d0f47f8730f6e140c970e8f96b62d355488998
Chatbot-ana <137336430+Chatbot-ana@users.noreply.github.com>
2023-07-07 10:28:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,34 +3,20 @@ document.body.style.zoom = "80%";
 document.addEventListener('DOMContentLoaded', function() {
   var botao = document.getElementsByClassName('botao')[0];
   var add = document.getElementById('add');
-  var audioFrame = document.getElementById('audio1');
   var alternarC = true;
-  const audioLink = document.querySelector('.audio');
 
   function alternar() {
     if (alternarC) {
       add.style.display = 'block';
       botao.style.width = '250px';
       botao.value = 'Sair da seção';
-      audioFrame.style.display = 'none'; // Esconde o áudio
-      toggleAudio(false); // Oculta o áudio
       alternarC = false;
     } else {
       add.style.display = 'none';
       botao.value = 'Entrar na seção (chat no lado direito)';
       botao.style.width = '420px';
-      audioFrame.style.display = 'flex'; // Mostra o áudio novamente
-      toggleAudio(true); // Mostra o áudio
       alternarC = true;
       recarregarPagina();
-    }
-  }
-
-  function toggleAudio(show) {
-    if (show) {
-      audioFrame.style.display = 'block'; // Mostra o áudio
-    } else {
-      audioFrame.style.display = 'none'; // Oculta o áudio
     }
   }
 
@@ -42,16 +28,18 @@ document.addEventListener('DOMContentLoaded', function() {
     location.reload();
   }
 
+  const audioLink = document.querySelector('.audio');
+  const audioFrame = document.getElementById('audio');
+
   audioLink.addEventListener('click', function(event) {
     event.preventDefault();
 
     if (audioFrame.style.display === 'none') {
-      toggleAudio(true); // Mostra o áudio
+      audioFrame.style.display = 'block';
       botao.style.display = 'none'; // Esconde o botão
     } else {
-      toggleAudio(false); // Oculta o áudio
+      audioFrame.style.display = 'none';
       botao.style.display = 'block'; // Mostra o botão novamente
     }
   });
 });
-
